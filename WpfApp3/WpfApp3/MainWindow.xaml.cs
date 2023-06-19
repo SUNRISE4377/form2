1630580e560e14bfa72c49cb20a23e45e4c11c53
SUNRISE4377 <fire0998@gmail.com>
2023-06-19 22:12:06 +0500

    form2

---
+++ WpfApp3/WpfApp3/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void OnChecked(object sender, RoutedEventArgs e)
+        {
+
+            Width = MaxWidth;
+            Height = MaxHeight;
+        }
+
+        private void OnUnchecked(object sender, RoutedEventArgs e)
+        {
+            Width = MinWidth;
+            Height = MinHeight;
+        }
+
+       
+    }
+}
